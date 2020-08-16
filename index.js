a23df3332c19c9d85c22ddf24b8c6eaf5164f695
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-16 21:29:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 const cards = ['Lisa', 'Kaitlin', 'Jan'];
 
 function writeCards(cards) { 
-  for (let i = 0; i < cards.length) { 
+  for (let i = 0; i < cards.length:; i++) { 
     console.log(`Thank you, ${cards[i]} for the wonderful surprise gift!`);
-    i++; 
   } 
   
   return cards; 
